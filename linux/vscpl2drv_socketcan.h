d3bbd6ba81f355b563c57d6ba4ebd5c7c131c777
Ake Hedman, Grodans Paradis AB <akhe@grodansparadis.com>
2019-10-10 18:08:50 +0200

    Now compiles again

--- linux/vscpl2drv_socketcan.h
+++ linux/vscpl2drv_socketcan.h
@@ -5,17 +5,17 @@
 // modify it under the terms of the GNU Lesser General Public License
 // as published by the Free Software Foundation; either version
 // 2 of the License, or (at your option) any later version.
-// 
-// This file is part of the VSCP (http://www.vscp.org) 
+//
+// This file is part of the VSCP (http://www.vscp.org)
 //
 // Copyright (C) 2000-2019 Ake Hedman,
 // Ake Hedman, Grodans Paradis AB, <akhe@grodansparadis.com>
-// 
+//
 // This file is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser  General Public License
 // along with this file see the file COPYING.  If not, write to
 // the Free Software Foundation, 59 Temple Place - Suite 330,
@@ -37,9 +37,9 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <syslog.h>
-#include "../../../../common/canal.h"
-#include "../../../../common/vscpremotetcpif.h"
-#include "../../../../common/canal_macro.h"
+#include <canal.h>
+#include <vscpremotetcpif.h>
+#include <canal_macro.h>
 
 #include "socketcan.h"
 
